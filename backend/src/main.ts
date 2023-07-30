35823d7d31f1178ebd94e43a093644d0b711e819
vladyslavbasko <baskov228322@gmail.com>
2023-07-30 15:01:09 +0300

    add login button to app tsx, try to fix origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: "*",
+  });
   await app.listen(3000);
 }
 bootstrap();
